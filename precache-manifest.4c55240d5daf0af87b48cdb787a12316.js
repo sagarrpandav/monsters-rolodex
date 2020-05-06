5bb3535e8ebb7f3590fa1a1f2900aacc6b9bae34
swiftcoder.gp <P@ndav91@GP>
2020-05-07 00:54:47 +0530

    Updates

---
+++ precache-manifest.4c55240d5daf0af87b48cdb787a12316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12cb9a7b64e7b271745450163981e1e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "30e887aed3077a39685c",
+    "url": "/monsters-rolodex/static/css/main.2aeca2fc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e887aed3077a39685c",
+    "url": "/monsters-rolodex/static/js/main.900634f8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);

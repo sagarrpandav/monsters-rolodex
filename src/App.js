192df846c487c8619ed09e8711a8a6a09f36f5d1
swiftcoder.gp <P@ndav91@GP>
2020-05-07 00:39:20 +0530

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+
+        //this.handleChange = this.handleChange.bind(this); Needed if arrow func not used
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                return res.json();
+            })
+            .then(userData => {
+                this.setState({monsters: userData})
+            })
+            .catch(err => {
+                throw err;
+            });
+    }
+
+    handleChange = (event) => {
+        this.setState({searchField: event.target.value})
+    };
+
+    render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster => {
+            return monster.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+
+        return (
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeHolder='Search Monsters'
+                    handleChange={this.handleChange}/>
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './search-box.styles.css';
+
+export const SearchBox = ({placeHolder, handleChange}) => {
+    return (
+        <input className='search' type='search' placeholder={placeHolder} onChange={handleChange}/>
+    )
+}
